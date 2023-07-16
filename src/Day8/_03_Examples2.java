6d2552bf6dd59716b63842b5019f1e21b78b7a46
Elvira <elvira.nzr@gmail.com>
2023-07-16 16:15:00 -0400

    day 8 pr

--- src/Day8/_03_Examples2.java
+++ src/Day8/_03_Examples2.java
@@ -6,6 +6,9 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
 
 /**
  * Go to https://www.facebook.com/
@@ -24,27 +27,57 @@ public class _03_Examples2 extends BaseDriver {
     public void test1(){
 
         driver.get("https://www.facebook.com/");
-        WebElement createaccount = driver.findElement(By.cssSelector("" +
-                "" +
-                "" +
-                "" +
-                "" +
-                ""));
-        createaccount.click();
+        System.out.println("facebook url --> " + driver.getCurrentUrl());
 
-        WebElement firstname= driver.findElement(By.cssSelector("input[id^='u_2'][id*='_b_']"));
-        firstname.sendKeys("jhdjdjd");
-        WebElement lastname =driver.findElement(By.cssSelector("input[id^=\"u_2\"][id*=\"_d_\"]"));
-        lastname.sendKeys("kjdklda");
+        WebElement newAccount = driver.findElement(By.xpath("//a[text()='Create new account']"));
+        newAccount.click();
 
-        WebElement gmail =driver.findElement(By.cssSelector("input[id^='u_'][id*='g']"));
+        WebElement name= driver.findElement(By.cssSelector("input[id^='u_'][id*='_b_']"));
+        name.sendKeys("elkekj");
 
-        WebElement comf =driver.findElement(By.cssSelector("//input[@name=\"reg_email_confirmation__\"] "));
-        Assert.assertFalse(comf.isDisplayed());
-        WebElement password =driver.findElement(By.xpath("//input[@id=\"password_step_input\"]"));
-        password.sendKeys("dasc424asA");
+        WebElement lastname= driver.findElement(By.cssSelector("input[id^='u_'][id*='_d_']"));
+        lastname.sendKeys("kjldkfd");
 
+        WebElement email= driver.findElement(By.cssSelector(" input[id^='u_'][id*='_g_']"));
 
+        WebElement emailConf= driver.findElement(By.cssSelector(" input[id^='u_'][id*='_j_']"));
+        Assert.assertFalse(emailConf.isDisplayed());
+        System.out.println(emailConf.getText());
+
+        email.sendKeys("asdd@gmail.com");
+        Assert.assertTrue(emailConf.isDisplayed());
+        System.out.println(emailConf.getText());
+        emailConf.sendKeys("asdd@gmail.com");
+
+        WebElement password = driver.findElement(By.xpath("//input[@class='inputtext _58mg _5dba _2ph-'][@id=\"password_step_input\"]"));
+        password.sendKeys("kjhdA355434");
+
+        WebElement month = driver.findElement(By.id("month"));
+        Select selectmonth = new Select(month);
+        List<WebElement> monthlist = selectmonth.getOptions();
+        int index= (int) (Math.random() * monthlist.size());
+        selectmonth.selectByIndex(index);
+        MyMethods.myWait(2);
+
+        WebElement day = driver.findElement(By.id("day"));
+        Select selectday = new Select(month);
+        List<WebElement> daylist = selectday.getOptions();
+        int index1= (int) (Math.random() * daylist.size());
+        selectmonth.selectByIndex(index1);
+        MyMethods.myWait(2);
+
+        WebElement year = driver.findElement(By.id("year"));
+        Select selectyear = new Select(year);
+        List<WebElement> yearlist = selectyear.getOptions();
+        int index2= (int) (Math.random() * yearlist.size());
+        selectyear.selectByIndex(index2);
+        MyMethods.myWait(2);
+
+        MyMethods.myWait(1);
+        WebElement gender = driver.findElement(By.xpath("//input[@value='1']"));
+        gender.click();
+
+        Assert.assertTrue(gender.isSelected());
 
         MyMethods.myWait(1);
         driver.quit();
